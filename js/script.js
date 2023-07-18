fc0c77a362a1c033a2ef6dd54b9abf0242a79f42
Ryan Venturino <adventurino@outlook.com>
2023-07-18 08:56:52 -0400

    router and first fetch

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+async function displayPopularMovies(){
+    const results = fetchAPIData('movie/popular')
+    console.log(results);
+}
+
+//Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'e66a94914cbf6515f34a7ccbea800fe8';
+  const API_URL = 'https://www.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+
+  return data;
+}
+
+//Highlight Active Link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+//Init app
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      displayPopularMovies();
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
